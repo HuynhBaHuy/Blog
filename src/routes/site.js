cfd3c7b7b91b1ffb356814a69a9037a2b983353f
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-11-01 18:56:01 +0700

    config husky

--- src/routes/site.js
+++ src/routes/site.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-router.use('/search', siteController.search);
+      router.use('/search', siteController.search);
 
 router.use('/', siteController.home);
 

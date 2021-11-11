95e6bc70d8fe173873f89045df3c8cb5a1388b19
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-11-11 17:47:04 +0700

    delete course

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const courseController = require('../app/controllers/CourseController');
 
 router.get('/:id/edit', courseController.edit);
+router.delete('/:id', courseController.destroy);
 router.put('/:id/update', courseController.update);
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
